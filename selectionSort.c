0ee8afe5cbe06e1c0f9e498dbe75ce6a5a78d612
Gabriel <gabrielsoares.prog@gmail.com>
2019-04-04 23:39:50 -0300

    Add shelSort

--- selectionSort.c
+++ selectionSort.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define DIM 80000
+#define DIM 1000
 
 void selectionSort (int vetor[], int tamVetor);
+void insertionSort(int vetor[]);
+void bubbleSort(int vetor[]);
+void shellSort(int vetor[]);
 void popularVetor (int vetor[]);
 void imprimirVetor (int vetor[]);
+void vetorOrdenado(int vetor[]);
+void comparacao(int vetor[]);
 
 int main()
 {
@@ -14,18 +19,21 @@ int main()
     size_t inicio, fim;
     double tempo;
 
-    printf("Vetor Desordenado:\n");
     popularVetor(vetor);
-    imprimirVetor(vetor);
-    printf("Vetor Ordenado:\n");
-    inicio = time(NULL);
-    selectionSort(vetor, tamVetor);
-    fim = time(NULL);
-    imprimirVetor(vetor); 
+    // imprimirVetor(vetor);
+    // shellSort(vetor);
+    // imprimirVetor(vetor);
+    //imprimirVetor(vetor);
+    // inicio = time(NULL);
+    // bubbleSort(vetor);
+    // fim = time(NULL);
 
-    tempo = difftime(fim, inicio);
+    // //imprimirVetor(vetor);
+    // tempo = difftime(fim,inicio);
+    // printf("Tempo de Execução: %.2lfs\n", tempo);
 
-    printf("Tempo de Execução: %.2lfs\n", tempo); 
+    comparacao(vetor);
+
 
     return 0;
 }
@@ -48,6 +56,57 @@ void selectionSort (int vetor[], int tamVetor){
     }
 }
 
+void insertionSort(int vetor[]){
+	int aux,i,j;
+	for(i=1;i<DIM;i++){
+		j = i;
+		while((j!=0) && (vetor[j] < vetor[j-1])){
+			aux = vetor[j];
+			vetor[j] = vetor[j-1];
+			vetor[j-1] = aux;
+			j--;
+		}
+		
+	}
+}
+
+void bubbleSort(int vetor[]){
+    int troca;
+    int aux;
+    do
+    {
+        troca = 0;
+        for(int i=0;i<DIM-1;i++){
+            if(vetor[i] > vetor[i+1]){
+                troca = 1;
+                aux = vetor[i];
+                vetor[i] = vetor[i+1];
+                vetor[i+1] = aux;
+
+            }
+        }
+    } while (troca == 1);
+    
+}
+
+void shellSort(int vetor[]){
+    int gap = DIM/2;
+    int valor, j;
+
+    while(gap>0){
+        for(int i=gap;i<DIM;i++){
+            valor = vetor[i];
+            j = i;
+            while(j>=gap && vetor[j-gap] > valor){
+                vetor[j] = vetor[j-gap];
+                j = j-gap;
+            }
+            vetor[j] = valor;
+        }
+        gap = gap/2;
+    }
+}
+
 void popularVetor(int vetor[]){
     srand( (unsigned)time(NULL) );
     for(int i=0;i<DIM;i++){
@@ -61,4 +120,64 @@ void imprimirVetor(int vetor[]){
         printf("%d ", vetor[i]);
     }
     printf("]\n");
+}
+
+void vetorOrdenado(int vetor[]){
+    for(int i=0;i<DIM;i++){
+        vetor[i] = i+1;
+    }
+}
+
+void comparacao(int vetor[]){
+    int teste, countSelection = 0,countInsertion = 0, countBubble = 0, countShell = 0;
+    double timeSelection, timeInsertion, timeBubble, timeShell;
+    clock_t inicio, fim;
+    printf("Digite quantos casos de teste deseja fazer: ");
+    scanf("%d", &teste);
+
+    for(int i=0;i<teste;i++){
+        popularVetor(vetor);
+        inicio = clock();
+        selectionSort(vetor, DIM);
+        fim = clock();
+        timeSelection = (float)(fim-inicio)/CLOCKS_PER_SEC;
+
+        popularVetor(vetor);
+        inicio = clock();
+        insertionSort(vetor);
+        fim = clock();
+        timeInsertion = (float)(fim-inicio)/CLOCKS_PER_SEC;
+
+        popularVetor(vetor);
+        inicio = clock();
+        bubbleSort(vetor);
+        fim = clock();
+        timeBubble = (float)(fim-inicio)/CLOCKS_PER_SEC;
+
+        popularVetor(vetor);
+        inicio = clock();
+        shellSort(vetor);
+        fim = clock();
+        timeShell = (float)(fim-inicio)/CLOCKS_PER_SEC;
+
+        if(timeSelection < timeInsertion && timeSelection < timeBubble && timeSelection < timeShell){
+            countSelection += 1;
+        }
+
+        if(timeInsertion < timeSelection && timeInsertion < timeBubble && timeInsertion < timeShell){
+            countInsertion += 1;
+        }
+
+        if(timeBubble < timeInsertion && timeBubble < timeSelection && timeBubble < timeShell){
+            countBubble += 1;
+        }
+
+        if(timeShell < timeInsertion && timeShell < timeSelection && timeShell < timeBubble){
+            countShell += 1;
+        }
+        printf("Selection(%f): %d\t Insertion(%f): %d\t Bubble(%f): %d, Shell(%f): %d\n",timeSelection, countSelection,timeInsertion,countInsertion,timeBubble, countBubble, timeShell, countShell);
+    }
+
+    printf("Pontuação Final\n");
+    printf("Selection: %d\t Insertion: %d\t Bubble: %d, Shell: %d\n",countSelection,countInsertion,countBubble,countShell);
 }
